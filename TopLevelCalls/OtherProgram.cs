b5f4328b06f4d80909dfcf5764b102bed7e84a9d
Jarne Van Aerde <jarne.vanaerde@hotmail.com>
2021-01-18 17:36:33 +0100

    Added TopLevelCalls project

--- TopLevelCalls/OtherProgram.cs
+++ TopLevelCalls/OtherProgram.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TopLevelCalls
 {
-    class OtherProgram
+    public class OtherProgram
     {
+        //public static void Main()
+        //{
+        //    Console.WriteLine("Hello world!");
+        //}
     }
 }
---
+++ TopLevelCalls/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TopLevelCalls
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello world!");
+            Console.WriteLine($"2 + 2 = {Add(2, 2)}");
+        }
+
+        private static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
