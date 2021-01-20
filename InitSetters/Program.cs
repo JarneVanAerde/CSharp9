869144ccbaeeb3ec67fac33d6de4497abb64c0f4
Jarne Van Aerde <jarne.vanaerde@hotmail.com>
2021-01-20 17:31:06 +0100

    Made new init project

---
+++ InitSetters/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using InitSetters;
+
+var person = new Person
+{
+    Id = 1,
+    FirstName = "Jarne",
+    LastName = "Van Aerde"
+};
+
+Console.WriteLine($"Hello {person.FirstName} {person.LastName} ({person.Id})");
