18cdfda1a221629c0c7aa341e679e8d2db3be1d6
Jarne Van Aerde <jarne.vanaerde@hotmail.com>
2021-02-01 18:43:42 +0100

    Do not do this

---
+++ Records/Record3.cs
@@ -0,0 +1,11 @@
+﻿namespace Records
+{
+    /// <summary>
+    /// DOE DIT NIET!!!!
+    /// </summary>
+    public record Record3
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
